05b1c6b6757cfcd05a20ec196ea2260c31256eb6
sujon3537 <119605414+sujon3537@users.noreply.github.com>
2025-09-05 10:25:39 +0600

    +api in config

--- next.config.js
+++ next.config.js
@@ -14,12 +14,12 @@ module.exports = (phase) => {
 
   const env = {
     API_PROD_URL: (() => {
-      if (isDev) return "http://localhost:3000/";
+      if (isDev) return "http://localhost:3000/api";
       if (isProd) {
         // Note: The code below needs to be uncommented, and you should use your domin where your API is hosted.
-        return "http://localhost:3000/";
+        return "http://localhost:3000/api";
       }
-      if (isStaging) return "http://localhost:3000/";
+      if (isStaging) return "http://localhost:3000/api";
       return "RESTURL_SPEAKERS:not (isDev,isProd && !isStaging,isProd && isStaging)";
     })(),
     API_BASE_URL: "http://localhost:3000/api",
